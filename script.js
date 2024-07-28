22f7b12cff0783f2a5a3f2af3a9c179295cd0628
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-07-27 20:35:42 -0400

    made function to create board in a array

---
+++ script.js
@@ -0,0 +1,24 @@
+function  gameboard() {
+    let board = [
+        [0,0,0],
+        [0,0,0],
+        [0,0,0]
+    ];
+
+    console.log(board)
+    console.log(board[0][2])
+}
+
+
+function player1() {
+    firstPlayer = 1;
+}
+
+function player2() {
+    secondPlayer = 2
+}
+
+gameboard();
+let game = {
+
+}
