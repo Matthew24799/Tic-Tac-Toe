5f0e1bbecf5d9960604e1a18baaa49325a7b99b9
Matthew24799 <157596330+Matthew24799@users.noreply.github.com>
2024-07-29 08:16:23 -0400

    created function to store player marks and board values

--- script.js
+++ script.js
@@ -1,24 +1,52 @@
 function  gameboard(board) {
-    board = [    
-     [0,0,0],   
-       [0,0,0],    
-       [0,0,0]  
-    ];
+    const rows = 3;
+    const cols = 3;
+    board = []
+
+
+        for (let i = 0; i < rows; i++) {
+            board[i] = [];
+
+            for (let j = 0; j < cols; j++) {
+                board[i].push(cell());
+            }
+        }
         
+
+        
+    
+    console.log(board)
+
    return board;
 }
 
-function gameLogic() {
-    let board = gameboard();
+
+function cell() {
+    let value = 0;
+
+    const addMark = (player) => {
+        value = player;
+    };
+
+    const getValue = () => value;
+
+    return {
+        addMark,
+        getValue
+    };
+  }
+
+function gameFlow() {
+    const board = gameboard();
 
 
     let players = [
         {
-            player: "playerOne",
+            name: "playerOne",
             mark: 1
         },
         {
-            player: "playerTwo",
+            name: "playerTwo",
             mark: 2
         }
 
@@ -29,13 +57,9 @@ function gameLogic() {
     const switchTurn = () => {
         activePlayer = activePlayer === players[0] ? players[1] : players[0]
     }
-    switchTurn();
-    console.log(activePlayer);
     
-    console.log(activePlayer)
-    switchTurn()
 }
 
-const game = gameLogic();
+const game = gameboard();
 
 
